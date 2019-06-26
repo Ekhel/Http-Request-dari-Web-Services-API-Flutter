aed94a5fa9bfd05076e57e9f7964fb81510daa8f
ekhel <blackmanskill@gmail.com>
2019-06-27 00:38:43 +0900

    FirstCommit

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Map data;
+  List userData;
+
+  Future getData() async {
+    http.Response response = await http.get("http://sickat.jayapurakab.go.id/api/get_realisasi_pd");
+    data = json.decode(response.body);
+    setState(() {
+     userData =  data["realisasi"];
+    });
+    debugPrint(userData.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Http Requet"),
+        backgroundColor: Colors.green,
+      ),
+      body: ListView.builder(
+        itemCount: userData==null ? 0 : userData.length,
+        itemBuilder: (BuildContext context, int index){
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("${userData[index]['PD']} ${userData[index]['PK']}"),
+                  ),
+                  
+                ],
+              ),
+            )
+          );
+        },
+      ),
+    );
+  }
+}
+
+
